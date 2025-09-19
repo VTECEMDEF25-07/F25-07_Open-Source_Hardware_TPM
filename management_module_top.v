7fcc9b1023166bd3634ee8735d2ad7c1dc91bebb
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-09-19 12:11:48 -0400

    Update management_module_top.v to align with new management module inputs and outputs

--- management_module_top.v
+++ management_module_top.v
@@ -16,35 +16,14 @@ module management_module_top(CLOCK_50, KEY, SW, HEX4, HEX3, HEX2, HEX1, HEX0, LE
 	wire [15:0] testsRunIn;
 	wire [15:0] testsPassedIn;
 	wire [15:0] untestedIn;
+	wire [31:0] ee_rc_in;
 	
 	wire [2:0] op_stateOut;
+	wire [1:0] startup_typeOut;
+	wire [31:0] tpm_rcOut;
+	wire shutdownSaveOut;
 	
-	wire shutdownSaveOut,
-		  platformAuthSelectOut, 
-		  platformPolicySelectOut,
-		  platformAlgSelectOut,
-		  nv_writeLockedSelectOut,
-		  nv_writtenSelectOut,
-		  nullProofGenEnableOut,
-		  nullSeedGenEnableOut,
-		  contextArraySelectOut,
-		  newContextEncryptionKeyEnableOut,
-		  commandAuditDigestSelectOut,
-		  commitNonceGenEnableOut,
-		  commitArraySelectOut,
-		  pcrSaveSelectOut,
-		  act_timeoutSelectOut,
-		  act_signaledSelectOut,
-		  act_authPolicySelectOut,
-		  act_hashAlgSelectOut;
-		  
-	wire [31:0] contextCountOut,
-					objectContextIDOut,
-					restartCountOut,
-					clearCountOut,
-					resetCountOut,
-					pcrUpdateCountOut,
-					commitCountOut;
+	wire s_initialized;
 					
 	reg  [3:0] hex4, hex3, hex2, hex1, hex0;
 	
@@ -52,6 +31,9 @@ module management_module_top(CLOCK_50, KEY, SW, HEX4, HEX3, HEX2, HEX1, HEX0, LE
 	
 	assign tpm_cc_in = {24'h1, SW[7:0]};
 	assign cc_param_in = {15'h0, SW[8]};
+	
+	assign ee_rc_in = 32'h0;
+	assign s_initialized = 1'b1;
 	
 	assign testsRunIn = 16'd40;
 	assign testsPassedIn = (SW[9] == 1'b1)? 16'd40 : 16'd3;
@@ -62,94 +44,26 @@ module management_module_top(CLOCK_50, KEY, SW, HEX4, HEX3, HEX2, HEX1, HEX0, LE
 	assign LED[8] = (op_stateOut == FAILURE_MODE_STATE);
 	assign LED[9] = (op_stateOut == SHUTDOWN_STATE);
 	
-	management_module u1(CLOCK_50, 
+	management_module2 u1(CLOCK_50, 
 		 KEY[0],
 		 KEY[1],
 		 tpm_cc_in,
 		 cc_param_in,
 		 1'b0,
+		 s_initialized,
+		 ee_rc_in,
 		 testsRunIn,
 		 testsPassedIn,
 		 untestedIn,
 		 op_stateOut,
-		 LED[4],
+		 startup_typeOut,
+		 tpm_rcOut,
 		 LED[3],
 		 LED[2],
 		 LED[1],
 		 LED[0],
-		 shutdownSaveOut,
-		 platformAuthSelectOut, 
-		 platformPolicySelectOut,
-		 platformAlgSelectOut,
-		 nv_writeLockedSelectOut,
-		 nv_writtenSelectOut,
-		 nullProofGenEnableOut,
-		 nullSeedGenEnableOut,
-		 contextArraySelectOut,
-		 contextCountOut,
-		 commandAuditDigestSelectOut,
-		 objectContextIDOut,
-		 newContextEncryptionKeyEnableOut,
-		 restartCountOut,
-		 clearCountOut,
-		 resetCountOut,
-		 pcrUpdateCountOut,
-		 commitCountOut,
-		 commitNonceGenEnableOut,
-		 commitArraySelectOut,
-		 pcrSaveSelectOut,
-		 act_timeoutSelectOut,
-		 act_signaledSelectOut,
-		 act_authPolicySelectOut,
-		 act_hashAlgSelectOut);
+		 shutdownSaveOut
+		 );
 		 
-	sevensegdecoder_proc_emmaw21 u2(hex4, HEX4);
-	sevensegdecoder_proc_emmaw21 u3(hex3, HEX3);
-	sevensegdecoder_proc_emmaw21 u4(hex2, HEX2);
-	sevensegdecoder_proc_emmaw21 u5(hex1, HEX1);
-	sevensegdecoder_proc_emmaw21 u6(hex0, HEX0);
-	
-	always@(op_stateOut) begin
-		case(op_stateOut)
-			POWER_OFF_STATE: 		 {hex4, hex3, hex2, hex1, hex0} = 20'h20FF5;
-			INITIALIZATION_STATE: {hex4, hex3, hex2, hex1, hex0} = 20'h17195;
-			STARTUP_STATE: 		 {hex4, hex3, hex2, hex1, hex0} = 20'h59A89;
-			OPERATIONAL_STATE: 	 {hex4, hex3, hex2, hex1, hex0} = 20'h02E89;
-			SELF_TEST_STATE: 		 {hex4, hex3, hex2, hex1, hex0} = 20'h5E6F9;
-			FAILURE_MODE_STATE: 	 {hex4, hex3, hex2, hex1, hex0} = 20'hFA165;
-			SHUTDOWN_STATE:		 {hex4, hex3, hex2, hex1, hex0} = 20'h53495;
-			default:		 			 {hex4, hex3, hex2, hex1, hex0} = 20'h00000;
-		endcase
-	end
-		 
-endmodule
+	endmodule
 
-/*module management_module_top(CLOCK_50, KEY, SW, HEX5, HEX4, HEX3, HEX2, HEX1, HEX0, LED);
-	input        CLOCK_50;
-	input  [3:0] KEY;
-	input  [9:0] SW;
-	output [6:0] HEX5;
-	output [6:0] HEX4;
-	output [6:0] HEX3;
-	output [6:0] HEX2;
-	output [6:0] HEX1;
-	output [6:0] HEX0;
-	output [9:0] LED;
-	
-	localparam OP_STATE = 9'd1, STARTUP = 9'd2, SELF_TEST = 9'd3, ENABLES = 9'd4, SELECTBITS = 9'd5, RESTART_COUNT = 9'd5, CLEAR_COUNT = 9'd5, RESET_COUNT = 9'd6;
-	
-	always@(SW) begin
-		case(SW)
-			OP_STATE:
-			STARTUP:
-			SELF_TEST:
-			ENABLES:
-			SELECTBITS:
-			RESTART_COUNT:
-			CLEAR_COUNT:
-			RESET_COUNT:
-			default:
-		endcase
-	end
-endmodule
-*/
