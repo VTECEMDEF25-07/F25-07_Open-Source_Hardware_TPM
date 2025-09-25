629200ae7603ade51bfa7f3c70562e01006ce518
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-09-25 19:12:16 -0400

    Update management_module.v to edited version after operational states were validated in testing.

--- management_module.v
+++ management_module.v
@@ -1,40 +1,43 @@
 // management_module.v
 
 module management_module(		 
-		 clock, 
-		 reset_n,
-		 keyStart_n,
-		 tpm_cc,
-		 cmd_param,
-		 orderlyInput,
-		 initialized,
-		 authHierarchy,
-		 executionEng_rc,
-		 locality,
-		 testsRun,
-		 testsPassed,
-		 untested,
-		 nv_phEnableNV,
-		 nv_shEnable,
-		 nv_ehEnable,
-		 op_state,
-		 startup_type,
-		 tpm_rc,
-		 phEnable,
-		 phEnableNV,
-		 shEnable,
-		 ehEnable,
-		 shutdownSave
-		 );
+		clock,						// Input clock signal
+		reset_n,						// Input reset signal
+		keyStart_n,
+
+		tpm_cc,					// 4-byte input command
+		cmd_param,				// 4086-byte input command parameters
+		
+		orderlyInput,
+		initialized,			// 1-bit input intialized bit (from execution engine)
+		authHierarchy,		// 4-byte input verifying which hiearchy was authorized (from execution engine)
+		executionEng_rc,	// 4-byte execution engine response code
+		locality,				// 8-bit input of current locality
+		testsRun,
+		testsPassed,
+		untested,
+		nv_phEnableNV,
+	   nv_shEnable,
+		nv_ehEnable,
+		
+		op_state,
+		startup_type,
+		tpm_rc,					// 4-byte response code
+		phEnable,					// 1-bit output platform hierarchy enable
+		phEnableNV,				// 1-bit output platform hiearchy NV memory enable
+		shEnable,					// 1-bit output owner hierarchy enable
+		ehEnable,					// 1-bit output privacy administrator hierarchy enable
+		shutdownSave			// 1-bit output shutdownType
+		);
 		 
 	input 		  clock;						// Input clock signal
 	input 		  reset_n;						// Input reset signal
 	input			  keyStart_n;
 
 	input  [31:0] tpm_cc;					// 4-byte input command
-	input  [33:0] cmd_param;				// 4086-byte input command parameters
-	
-	input			  orderlyInput;
+	input  [32:0] cmd_param;				// 4086-byte input command parameters
+		
+	input	 [15:0] orderlyInput;
 	input 		  initialized;			// 1-bit input intialized bit (from execution engine)
 	input	 [31:0] authHierarchy;		// 4-byte input verifying which hiearchy was authorized (from execution engine)
 	input  [31:0] executionEng_rc;	// 4-byte execution engine response code
@@ -42,16 +45,18 @@ module management_module(
 	input	 [15:0] testsRun;
 	input	 [15:0] testsPassed;
 	input	 [15:0] untested;
-	input nv_phEnableNV, nv_shEnable, nv_ehEnable;
-	
+	input 		  nv_phEnableNV;
+	input	        nv_shEnable;
+	input         nv_ehEnable;
+		
 	output [2:0]  op_state;
-	output [1:0]  startup_type;
+	output [2:0]  startup_type;
 	output [31:0] tpm_rc;					// 4-byte response code
 	output 		  phEnable;					// 1-bit output platform hierarchy enable
 	output 		  phEnableNV;				// 1-bit output platform hiearchy NV memory enable
 	output 		  shEnable;					// 1-bit output owner hierarchy enable
 	output 		  ehEnable;					// 1-bit output privacy administrator hierarchy enable
-	output [15:0]  shutdownSave;			// 1-bit output shutdownType
+	output [15:0] shutdownSave;			// 1-bit output shutdownType
 	
 	
 	// Relevant Command Codes to Management module
@@ -84,19 +89,19 @@ module management_module(
 				  TPM_RH_PLATFORM_NV = 32'h4000000D;
 	
 	// Startup types
-	localparam TPM_DONE = 2'd0, TPM_RESET = 2'd1, TPM_RESTART = 2'd2, TPM_RESUME = 2'd3;
+	localparam TPM_DONE = 3'd0, TPM_RESET = 3'd1, TPM_RESTART = 3'd2, TPM_RESUME = 3'd3, TPM_TYPE = 3'd4;
 	
 	// Operational states
 	localparam POWER_OFF_STATE = 3'b000, INITIALIZATION_STATE = 3'b001, STARTUP_STATE = 3'b010, OPERATIONAL_STATE = 3'b011, SELF_TEST_STATE = 3'b100, FAILURE_MODE_STATE = 3'b101, SHUTDOWN_STATE = 3'b110;	// Operational states
 	
 	reg pHierarchy, pHierarchyNV, sHierarchy, eHierarchy;
-	reg phEnable, phEnableNV, shEnable, ehEnable, tpmi_yes_no, authPassed;
+	reg phEnable, phEnableNV, shEnable, ehEnable, tpmi_yes_no, authPassed, s_initialized;
 	
 	reg [15:0] startup_state, shutdown_input, shutdownSave;
 		 
-	reg [1:0] startup_type;
+	reg [2:0] startup_type;
 	reg [2:0] op_state, state;
-	reg [31:0] tpm_rc, tpm_rc_state, tpmi_rh_enables, tpmi_rh_hierarchy;
+	reg [31:0] tpm_rc_state, tpmi_rh_enables, tpmi_rh_hierarchy, tpm_rc;
 	
 	wire startupEnable, operationEnable, shutdownEnable;
 	
@@ -116,10 +121,13 @@ module management_module(
 				phEnableNV   <= pHierarchyNV;
 				shEnable     <= sHierarchy;
 				ehEnable     <= eHierarchy;
+				
 				tpm_rc		 <= tpm_rc_state;
 				if(startupEnable) begin
 					startup_state <= cmd_param[15:0];
 					shutdown_input <= orderlyInput;
+					s_initialized <= initialized;
+					
 				end
 				else if(operationEnable) begin
 					tpmi_rh_hierarchy <= authHierarchy;
@@ -134,13 +142,13 @@ module management_module(
 	end
 
 	// Enable signals to activate input information collection at startup and shutdown stages
-	assign startupEnable = (state == STARTUP_STATE);
-	assign operationEnable = (state == OPERATIONAL_STATE);
-	assign shutdownEnable = (state == SHUTDOWN_STATE);
+	assign startupEnable = (op_state == STARTUP_STATE);
+	assign operationEnable = (op_state == OPERATIONAL_STATE);
+	assign shutdownEnable = (op_state == SHUTDOWN_STATE);
 	
 	// Always block for managing operatonal states FSM
 	
-	always@(tpm_cc, op_state, initialized, cmd_param, untested, testsPassed, testsRun) begin
+	always@(tpm_cc, op_state, s_initialized, startup_type, cmd_param, untested, testsPassed, testsRun) begin
 		case(op_state)
 			POWER_OFF_STATE: 		 begin
 											 state = INITIALIZATION_STATE;
@@ -154,11 +162,14 @@ module management_module(
 											 end
 										 end
 			STARTUP_STATE:			 begin
-											 if(initialized == 1'b1) begin
+											 if(s_initialized == 1'b1) begin
 												state = OPERATIONAL_STATE;
 											 end
-											 else begin
+											 else if(startup_type == TPM_TYPE) begin
 												state = INITIALIZATION_STATE;
+											 end
+											 else begin
+												state = STARTUP_STATE;
 											 end
 										 end
 			OPERATIONAL_STATE: 	 begin
@@ -200,7 +211,7 @@ module management_module(
 											 state = FAILURE_MODE_STATE;
 										 end
 			SHUTDOWN_STATE:		 begin
-											 state = SHUTDOWN_STATE;
+											 state = OPERATIONAL_STATE;
 										 end
 			default:					 begin
 											 state = 3'bxxx;
@@ -210,7 +221,7 @@ module management_module(
 	
 	
 	//Always block for managing response codes
-	always@(tpm_rc, tpm_cc, op_state, shutdown_input, startup_state, initialized, executionEng_rc, locality, tpmi_rh_hierarchy, tpmi_rh_enables, tpmi_yes_no, untested, testsPassed, testsRun) begin
+	always@(tpm_rc, tpm_cc, op_state, shutdown_input, startup_state, s_initialized, executionEng_rc, locality, tpmi_rh_hierarchy, tpmi_rh_enables, tpmi_yes_no, untested, testsPassed, testsRun) begin
 		tpm_rc_state = tpm_rc;
 		if(op_state == INITIALIZATION_STATE) begin
 			if(tpm_cc != TPM_CC_STARTUP) begin
@@ -222,7 +233,7 @@ module management_module(
 				tpm_rc_state = TPM_RC_VALUE;
 			end
 											 
-			if(initialized == 1'b1) begin
+			if(s_initialized == 1'b1) begin
 				tpm_rc_state = TPM_RC_SUCCESS;
 			end
 		end
@@ -299,7 +310,7 @@ module management_module(
 			end
 			else begin
 				if(startup_state == TPM_SU_STATE) begin
-					startup_type = TPM_DONE;
+					startup_type = TPM_TYPE;
 				end
 				else begin
 					startup_type = TPM_RESET;
@@ -364,4 +375,5 @@ module management_module(
 	end
 	
 endmodule
+
 
