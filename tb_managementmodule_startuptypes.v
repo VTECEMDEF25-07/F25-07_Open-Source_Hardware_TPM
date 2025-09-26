04a47cfa7f8186dc39f85a60ddad3239da9d23e5
ewallace21 <111802680+ewallace21@users.noreply.github.com>
2025-09-26 18:01:25 -0400

    Update tb_managementmodule_startuptypes.v to fix timing

--- tb_managementmodule_startuptypes.v
+++ tb_managementmodule_startuptypes.v
@@ -20,36 +20,36 @@
 
 module tb_managementmodule_startuptypes();
 // Declare local wires and regs
-   reg         clk_enable;	// Locally-created clock enable signal
-   wire        clk_out;		// Clock produced by the clk module
+   reg         clk_enable;				// Locally-created clock enable signal
+   wire        clk_out;					// Clock produced by the clk module
 	
-   reg         tpm_reset_n;	// Locally-created active-low TPM reset signal
-   reg         tpm_enable_n;	// Locally-created active-low tpm enable signal for testing
-	reg  [31:0] tpm_cc_in;
-	reg  [32:0] cmd_param_in;
-	reg  [15:0] orderlyInput_in;
-	reg			initialized_in;
-	reg  [31:0] authHierarchy_in;		// 4-byte input verifying which hiearchy was authorized (from execution engine)
-	reg  [31:0] executionEng_rc_in;	// 4-byte execution engine response code
-	reg  [7:0]  locality_in;				// 8-bit input of current locality
-	reg  [15:0] testsRun_in;
-	reg  [15:0] testsPassed_in;
-	reg  [15:0] untested_in;
-	reg 		   nv_phEnableNV_in;			// Non-Volatile memory of last phEnableNV state
-	reg	      nv_shEnable_in;			// Non-Volatile memory of last shEnable state
-	reg         nv_ehEnable_in;			// Non-Volatile memory of last ehEnable state
+   reg         tpm_reset_n;			// Locally-created active-low TPM reset signal
+   reg         tpm_enable_n;			// Locally-created active-low tpm enable signal for testing
+	reg  [31:0] tpm_cc_in;				// Locally-created command code input signal
+	reg  [32:0] cmd_param_in;			// Locally-created command parameters input signal
+	reg  [15:0] orderlyInput_in;		// Locally-created orderly shutdown input signal from NV memory
+	reg			initialized_in;		// Locally-created initialized input signal from execution engine
+	reg  [31:0] authHierarchy_in;		// Locally-created 4-byte input verifying which hiearchy was authorized (from execution engine)
+	reg  [31:0] executionEng_rc_in;	// Locally-created 4-byte execution engine response code input
+	reg  [7:0]  locality_in;			// Locally-created 8-bit input of current locality
+	reg  [15:0] testsRun_in;			// Locally-created input from self-test module of amount of tests run
+	reg  [15:0] testsPassed_in;		// Locally-created input from self-test module of amount of tests passed
+	reg  [15:0] untested_in;			// Locally-created input from self-test module of amount of tests not run
+	reg 		   nv_phEnableNV_in;		// Locally-created input of Non-Volatile memory of last phEnableNV state
+	reg	      nv_shEnable_in;		// Locally-created input of Non-Volatile memory of last shEnable state
+	reg         nv_ehEnable_in;		// Locally-created input of Non-Volatile memory of last ehEnable state
 								
-	wire [2:0]  op_state_out;
-	wire [2:0]  startup_type_out;
-	wire [31:0] tpm_rc_out;					// 4-byte response code
-	wire 		   phEnable_out;					// 1-bit output platform hierarchy enable
-	wire 		   phEnableNV_out;				// 1-bit output platform hiearchy NV memory enable
-	wire 		   shEnable_out;					// 1-bit output owner hierarchy enable
-	wire 		   ehEnable_out;					// 1-bit output privacy administrator hierarchy enable
-	wire [15:0] shutdownSave_out;			// 1-bit output shutdownType
+	wire [2:0]  op_state_out;			// 3-bit operational state output to execution engine
+	wire [2:0]  startup_type_out;		// 3-bit startup type output to execution engine
+	wire [31:0] tpm_rc_out;				// 4-byte output response code
+	wire 		   phEnable_out;			// 1-bit output platform hierarchy enable
+	wire 		   phEnableNV_out;		// 1-bit output platform hiearchy NV memory enable
+	wire 		   shEnable_out;			// 1-bit output owner hierarchy enable
+	wire 		   ehEnable_out;			// 1-bit output privacy administrator hierarchy enable
+	wire [15:0] shutdownSave_out;		// 1-bit output shutdownType
 	
 
-// Instantiate the clock generator with a period of 100 ns
+// Instantiate the clock generator with a period of 20 ns
    clk CLK1(clk_enable, clk_out);
 
 // Instantiate the management module
@@ -105,23 +105,23 @@ module tb_managementmodule_startuptypes();
 
 	 // Test 0: TPM_RESET
 	 // Change operational state to INITIALIZATION_STATE to set up management module for testing.
-    #100;
+    #40;
 	 tpm_reset_n = 1'b0;
 	 #20;
     tpm_reset_n = 1'b1;			// Apply reset
 	 
 	 #20;
 	 tpm_enable_n = 1'b0; 		// Send enable low
-    #60;
+    #20;
     tpm_enable_n = 1'b1; 		// Send enable high
 	
 	 // Issue STARTUP command
-    #40;
+    #20;
     tpm_cc_in = 32'h00000144; // TPM_CC_STARTUP
     cmd_param_in = 33'd0;     // startup_state = TPM_SU_CLEAR
     orderlyInput_in = 16'd0;	// shutdown_input = TPM_SU_CLEAR
 	 tpm_enable_n = 1'b0; 		// Send enable low
-    #100;
+    #40;
     tpm_enable_n = 1'b1; 		// Send enable high
 	 // Expected Result: startup_type_out == 3'b001 (changes to TPM_RESET)
 	 
@@ -129,23 +129,23 @@ module tb_managementmodule_startuptypes();
 	 
 	 // Test 1: TPM_RESTART
 	 // Change operational state to INITIALIZATION_STATE to set up management module for testing.
-    #100;
+    #40;
 	 tpm_reset_n = 1'b0;
 	 #20;
     tpm_reset_n = 1'b1;			  // Apply reset
 	 
 	 #20;
 	 tpm_enable_n = 1'b0; 		  // Send enable low
-    #60;
+    #20;
     tpm_enable_n = 1'b1; 		  // Send enable high
 	
 	 // Issue STARTUP command
-    #40;
+    #20;
     tpm_cc_in = 32'h00000144;   // TPM_CC_STARTUP
     cmd_param_in = 33'd0;       // startup_state = TPM_SU_CLEAR
     orderlyInput_in = 16'h0001; // shutdown_input = TPM_SU_STATE
 	 tpm_enable_n = 1'b0;        // Send enable low
-    #100;
+    #40;
     tpm_enable_n = 1'b1; 		  // Send enable high
 	 // Expected Result: startup_type_out == 3'b010 (changes to TPM_RESTART)
 	 
@@ -153,23 +153,23 @@ module tb_managementmodule_startuptypes();
 	 
 	 // Test 2: TPM_TYPE (Fails to set a startup type)
 	 // Change operational state to INITIALIZATION_STATE to set up management module for testing.
-    #100;
+    #40;
 	 tpm_reset_n = 1'b0;
 	 #20;
     tpm_reset_n = 1'b1;			  		// Apply reset
 	 
 	 #20;
 	 tpm_enable_n = 1'b0; 		  		// Send enable low
-    #60;
+    #20;
     tpm_enable_n = 1'b1; 		  		// Send enable high
 	
 	 // Issue STARTUP command
-    #40;
+    #20;
     tpm_cc_in = 32'h00000144;   		// TPM_CC_STARTUP
     cmd_param_in = 33'h000000001;   // startup_state = TPM_SU_STATE
     orderlyInput_in = 16'h0001; 		// shutdown_input = TPM_SU_STATE
 	 tpm_enable_n = 1'b0;        		// Send enable low
-    #100;
+    #40;
     tpm_enable_n = 1'b1; 		  		// Send enable high
 	 // Expected Result: startup_type_out == 3'b011 (changes to TPM_RESTART)
 	 
@@ -177,28 +177,28 @@ module tb_managementmodule_startuptypes();
 	 
 	 // Test 3: TPM_RESET
 	 // Change operational state to INITIALIZATION_STATE to set up management module for testing.
-    #100;
+    #40;
 	 tpm_reset_n = 1'b0;
 	 #20;
     tpm_reset_n = 1'b1;					// Apply reset
 	 
 	 #20;
 	 tpm_enable_n = 1'b0; 				// Send enable low
-    #60;
+    #20;
     tpm_enable_n = 1'b1; 				// Send enable high
 	
 	 // Issue STARTUP command
-    #40;
+    #20;
     tpm_cc_in = 32'h00000144; 		// TPM_CC_STARTUP
     cmd_param_in = 33'h000000001;   // startup_state = TPM_SU_STATE
     orderlyInput_in = 16'd0;			// shutdown_input = TPM_SU_CLEAR
 	 tpm_enable_n = 1'b0; 				// Send enable low
-    #100;	
+    #40;	
     tpm_enable_n = 1'b1; 				// Send enable high
 	 
-	 #40;
+	 #20;
 	 tpm_enable_n = 1'b0; 				// Send enable low
-    #60;	
+    #20;	
     tpm_enable_n = 1'b1; 				// Send enable high
 	 
 	 
